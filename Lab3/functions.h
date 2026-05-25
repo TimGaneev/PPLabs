9f36cbe129f036b25eb9f7a43afd90750954c2b8
TimGaneev <tim.ganeev718@gmail.com>
2026-05-25 20:01:07 +0400

    Lab3

---
+++ Lab3/functions.h
@@ -0,0 +1,148 @@
+#ifndef MY_FUNCTIONS_H
+#define MY_FUNCTIONS_H
+
+#include <iostream>
+#include <random>
+#include <ctime>
+#include <fstream>
+#include <filesystem>
+#include "mpi.h"
+#include "Matrix.h"
+
+ 
+void setup_io()
+{
+#ifdef _MSC_VER
+  system("chcp 65001 > nul");
+#endif
+
+  std::ios_base::sync_with_stdio(false);
+}
+
+//Генерация квадратной матрицы
+Matrix generate_random_matrix(size_t size)
+{
+  std::random_device seed;
+  std::default_random_engine e(seed());
+  std::uniform_int_distribution d(-100, 100);
+
+  Matrix m(size);
+  for(int i = 0;i<size;i++)
+  {
+    for (int j = 0;j < size;j++)
+    {
+      *m(i, j) = d(e);
+    }
+  }
+  return m;
+}
+
+//Сохранение матрицы в файл
+void save_matrix(std::filesystem::path file_name, const Matrix& m)
+{
+  std::filesystem::path directorypath = "../../../matrix_size_" + std::to_string(m.size());
+  if (!std::filesystem::exists(directorypath))
+  {
+    std::filesystem::create_directory(directorypath);
+  }
+  std::ofstream file(directorypath / file_name);
+  file << m.size() << "\n" << m;
+}
+
+//Чтение матрицы из файла
+Matrix read_matrix(std::filesystem::path file_name, size_t size)
+{
+  std::filesystem::path directorypath = "../../../matrix_size_" + std::to_string(size);
+  if (!std::filesystem::exists(directorypath / file_name)) { throw "file not found"; }
+
+  std::ifstream file(directorypath / file_name);
+  if (!file.is_open()) { throw "unable to open file"; }
+  file >> size;
+  Matrix m(size);
+
+  for (int i = 0; i < size; i++)
+  {
+    for (int j = 0; j < size; j++)
+    {
+      file >> *m(i, j);
+    }
+  }
+  return m;
+}
+
+/*
+Перемножение матриц и сохранение
+  размеров исходных/полученной матриц
+  времени вычисления
+  числа операций
+*/
+Matrix multiply(int argc, char* argv[], const Matrix& lhs, const Matrix& rhs)
+{
+  if (lhs.size() != rhs.size()) { throw "Matrix sizes dont match"; }
+
+  std::clock_t start = clock();
+  double duration = 0;
+  int size, rank, n = lhs.size(), i, j, k, temp;
+  int* a = new int[n * n];
+  int* b = new int[n * n];
+  int rem;
+  Matrix result(n);
+
+  MPI_Init(&argc, &argv);
+  MPI_Comm_size(MPI_COMM_WORLD, &size);
+  MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+
+  rem = n % size;
+
+  for (i = 0; i < n/size; i++)
+  {
+    for (j = 0; j < n; j++)
+    {
+      temp = 0;
+      for (k = 0; k < n; k++)
+      {
+        temp += lhs(i + rank * (n / size), k) * rhs(k, j);
+      }
+      b[i * n + j] = temp;
+    }
+  }
+
+  MPI_Gather(b, n * (n / size), MPI_INT, result(0, 0), n * (n / size), MPI_INT, 0, MPI_COMM_WORLD);
+  if (rank == 0)
+  {
+    for(i = 0; i < rem; i++)
+    {
+      for (j = 0; j < n; j++)
+      {
+        temp = 0;
+        for (k = 0; k < n; k++)
+        {
+          temp += lhs(n-rem+i, k) * rhs(k, j);
+        }
+        *result(n - rem + i, j) = temp;
+      }
+    }
+  }
+  MPI_Barrier(MPI_COMM_WORLD);
+
+  duration = double(clock() - start) / CLOCKS_PER_SEC;
+  std::cout << result.size() << ", " << rank << ", " << size << ", " << duration << "\n";
+
+  MPI_Barrier(MPI_COMM_WORLD);
+  if (rank == 0)
+  {
+   std::filesystem::path directorypath = "../../..";
+   std::fstream file(directorypath / (std::to_string(size) + "_data.csv"), std::ios::app);
+   if (std::filesystem::is_empty(directorypath / (std::to_string(size) + "_data.csv")))
+   {
+     file << "size," << "calculation time\n";
+   }
+   file << result.size() << ", " << duration << "\n";
+   save_matrix("result.txt", result);
+  }
+
+  MPI_Finalize();
+  return result;
+}
+
+#endif
---
+++ Lab3/visualise_graph.py
@@ -0,0 +1,64 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+
+#Чтение матрицы из файла
+def read_matrix(size: int, file_name: str) -> list:
+    with open("matrix_size_"+str(size)+"/"+file_name, "r", encoding="utf-8") as file:
+        file.readline()
+        matrix = list(map(int, file.read().split()))
+        return [np.array_split(matrix, size), size]
+
+
+#Нахождение среднего времени вычисления при разных размерах матриц
+def process_data(base_data: pd.DataFrame, sizes: list[int]) -> pd.DataFrame:
+    data = []
+    for size in sizes:
+        line = base_data[base_data["size"]==size].mean()
+        data.append(line)
+    return pd.DataFrame(data)
+
+
+#Отображение обработанных данных на графике, сохранение графика
+def display_graph(data_list: dict[int, pd.DataFrame], col: str, save_path: str) -> None:
+    for proc in data_list.keys():
+      plt.plot(data_list[proc]["size"], data_list[proc]["calculation time"],  label="processes: " + str(proc), marker="o")
+    plt.grid(True)
+    plt.xlabel("Matrix size")
+    plt.ylabel(col)
+    plt.legend()
+    plt.savefig(save_path)
+    plt.show()
+    plt.clf()
+    return
+
+
+def main() -> None:
+    try:
+        #Проверка корректности вычислений
+        sizes = [100, 200, 500, 1000, 2000]
+        for size in sizes:
+            matrix_a, size_a = read_matrix(size, "matrix_A.txt")
+            matrix_b, size_b = read_matrix(size, "matrix_B.txt")
+            my_result, size_r = read_matrix(size, "result.txt")
+            correct_result = np.dot(matrix_a, matrix_b)
+            if np.array_equal(my_result, correct_result):
+              print("Size ", size, ": all good")
+            else:
+              print("Size ", size, ": smth aint right")
+
+        #Обработка данных после перемножения матриц
+        processes = [1, 2, 4, 8, 12]
+        processed_data = {}
+        for proc in processes:
+          data = pd.read_csv(str(proc)+"_data.csv")
+          processed_data[proc] = process_data(data, sizes)
+        print(processed_data)
+        display_graph(processed_data, "calculation time", "time_graph.jpg")
+    except Exception as exc:
+      print(f"Возникла ошибка: {exc}")
+
+
+if __name__ == "__main__":
+    main()
