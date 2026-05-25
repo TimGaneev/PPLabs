1e432c0a0a624f3bbb349843cc4822350c266e34
TimGaneev <tim.ganeev718@gmail.com>
2026-05-25 23:09:16 +0400

    Lab5

---
+++ Lab5/main.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+#include "functions.h"
+#include "Matrix.h"
+
+
+using namespace std;
+
+int main(int argc, char* argv[]) {
+
+  setup_io();
+
+  try
+  {
+    if (argc < 2) { exit(0); }
+    int size = stoi(argv[1]);
+    Matrix a = read_matrix("matrix_A.txt", size);
+    Matrix b = read_matrix("matrix_b.txt", size);
+    Matrix c(b.size());
+    c = multiply(argc, argv, a, b);
+  }
+  catch (const char* e)
+  {
+    cerr << e;
+  }
+  return 0;
+}
